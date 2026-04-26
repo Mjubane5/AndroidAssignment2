a85a8c3fc34a64f32752291235d28d73f06053d6
Lugayenianele <anelelugayeni04@gmail.com>
2026-04-26 22:59:39 +0200

    feat(storage): implement global Inventory list and wire AddBookActivity to save data

---
+++ app/src/main/java/com/example/textbookmarketplace/Inventory.java
@@ -0,0 +1,8 @@
+package com.example.textbookmarketplace;
+
+import java.util.ArrayList;
+
+public class Inventory {
+    // This is a global list that will hold every single book anyone types in!
+    public static ArrayList<Textbook> textbookList = new ArrayList<>();
+}
